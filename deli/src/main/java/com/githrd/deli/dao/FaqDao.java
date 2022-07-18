bb8b1dfaa95b93668d92603ff5202b661f9dd394
Kohaneul <96707563+Kohaneul@users.noreply.github.com>
2022-07-19 00:13:56 +0900

    0719 commit

---
+++ deli/src/main/java/com/githrd/deli/dao/FaqDao.java
@@ -0,0 +1,38 @@
+package com.githrd.deli.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.githrd.deli.vo.faqVO;
+
+	public class FaqDao {
+
+		
+		@Autowired
+		private SqlSessionTemplate sqlSession;
+
+		public faqVO selectNum(int no) {
+			return sqlSession.selectOne("FDao.selectNum",no);
+		}
+		public List<faqVO> selectList() {
+			List<faqVO> list = sqlSession.selectList("FDao.selectList");
+			return list;
+		}
+		public int insert(faqVO vo) {
+			System.out.println("***insert");
+			return sqlSession.insert("FDao.insert", vo);
+		}
+		
+		public int update(faqVO vo) {
+			return sqlSession.update("FDao.update",vo);
+		}
+		
+		public int delete(int no) {
+			return sqlSession.delete("FDao.delete", no);
+		}
+		
+		
+		
+}
