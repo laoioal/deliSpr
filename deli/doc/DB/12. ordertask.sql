bf7d9fd9af9e8918f2cdd2c304023803510a5dca
AnEunbee <AnEunbee@DESKTOP-8CBASVO>
2022-07-22 18:37:10 +0900

    del

--- deli/doc/DB/12. ordertask.sql
+++
@@ -1,9 +0,0 @@
---delivery
-CREATE TABLE ordertask(
-    ono VARCHAR2(40CHAR)
-        CONSTRAINT OD_ONO_PK PRIMARY KEY,
-    mno NUMBER(4)
-        CONSTRAINT OD_MNO_FK REFERENCES member(mno)
-        CONSTRAINT OD_MNO_NN NOT NULL
-);
-
